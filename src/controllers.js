4d2902195f185c3e487f1426f754be9de6c8bd44
Marco Damaceno <maadamaceno@gmail.com>
2020-01-13 20:17:15 -0300

    feat: organize social icons

--- src/controllers.js
+++ src/controllers.js
@@ -17,7 +17,9 @@ const controllers = (request, response) => {
         lang: DEFAULT_LANG,
         title: APP_NAME,
         content:
-          getComponent('my-description') +
+          getComponent('my-description', {
+            social: `<a href="${process.env.PROFILE_GITHUB}" target="_blank"><ion-icon class="social-icon" name="logo-github"></ion-icon></a> <a href="${process.env.PROFILE_LINKEDIN}" target="_blank"><ion-icon class="social-icon" name="logo-linkedin"></ion-icon></a>`,
+          }) +
           getComponent('navbar') +
           getComponent('blog', {
             posts: posts.reduce((html, post) => {
@@ -45,7 +47,12 @@ const controllers = (request, response) => {
         description:
           metadata && metadata.description ? metadata.description : '',
         content:
-          getComponent('post/navbar') +
+          getComponent('post/navbar', { social: `<li class="nav-item">
+            <a class="nav-link active" href="${process.env.PROFILE_GITHUB}" target="_blank"><ion-icon class="social-icon" name="logo-github"></ion-icon></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="${process.env.PROFILE_LINKEDIN}" target="_blank"><ion-icon class="social-icon" name="logo-linkedin"></ion-icon></a>
+          </li>` }) +
           getComponent('post/post', {
             post: html,
             title: metadata.title,
